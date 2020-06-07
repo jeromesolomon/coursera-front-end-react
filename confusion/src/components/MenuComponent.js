e081e903b3926ccf6c5ef98f8c06f9d0290d2e64
Gabriel Jerome Solomon <gsolomon@scu.edu>
2020-06-06 17:27:08 -0700

    Added new empty component

---
+++ confusion/src/components/MenuComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+
+    constructo(props) {
+
+        super(props);
+
+
+    }
+
+    render() {
+        return (
+
+        );
+    }
+
+}
+
+export default Menu;
