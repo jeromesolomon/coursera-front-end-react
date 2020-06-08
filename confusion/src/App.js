715fc5604b695a53328deeab9e0e9796348a54e2
Gabriel Jerome Solomon <gsolomon@scu.edu>
2020-06-08 15:02:21 -0700

    Added data to app state, and passed to children via props

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+
+// shared data
+import { DISHLIST } from './shared/dishList';
 
 // import new components
 import Menu from './components/MenuComponent';
 
 import './App.css';
 
-// 
+class App extends Component {
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu></Menu>
-    </div>
-  );
+  constructor(props) {
+
+    super(props);
+
+    // add dish list to the apps state
+    this.state = {
+      dishes: DISHLIST
+    }
+
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishList={this.state.dishes}></Menu>
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
+import { DISHLIST } from '../shared/dishList';
+
+
 
 class Menu extends Component {
 
@@ -8,42 +11,7 @@ class Menu extends Component {
         super(props);
 
         // state has properties related to this component
-
         this.state = {
-            dishList: [
-                {
-                  id: 0,
-                  name:'Uthappizza',
-                  image: 'assets/images/uthappizza.png',
-                  category: 'mains',
-                  label:'Hot',
-                  price:'4.99',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-               {
-                  id: 1,
-                  name:'Zucchipakoda',
-                  image: 'assets/images/zucchipakoda.png',
-                  category: 'appetizer',
-                  label:'',
-                  price:'1.99',
-                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-               {
-                  id: 2,
-                  name:'Vadonut',
-                  image: 'assets/images/vadonut.png',
-                  category: 'appetizer',
-                  label:'New',
-                  price:'1.99',
-                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-               {
-                  id: 3,
-                  name:'ElaiCheese Cake',
-                  image: 'assets/images/elaicheesecake.png',
-                  category: 'dessert',
-                  label:'',
-                  price:'2.99',
-                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-               ]
         };
 
     }
@@ -51,7 +19,7 @@ class Menu extends Component {
     render() {
 
         // construct a list of media items
-        const menu = this.state.dishList.map(
+        const menu = this.props.dishList.map(
             (dish, index) => {
 
                 return (
