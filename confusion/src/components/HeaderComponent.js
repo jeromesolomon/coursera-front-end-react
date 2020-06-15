380530347cebfb9b32f738cc2edb23c9940f0ac0
Gabriel Jerome Solomon <gsolomon@scu.edu>
2020-06-14 17:51:56 -0700

    Added contact page

--- confusion/src/components/HeaderComponent.js
+++ confusion/src/components/HeaderComponent.js
@@ -31,16 +31,16 @@ class Header extends Component {
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                             <NavItem>
-                                <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                                <NavLink className="nav-link"  to='/home'><i className="fa fa-home fa-lg"></i> Home</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                                <NavLink className="nav-link" to='/aboutus'><i className="fa fa-info fa-lg"></i> About Us</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                                <NavLink className="nav-link"  to='/menu'><i className="fa fa-list fa-lg"></i> Menu</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                                <NavLink className="nav-link" to='/contactus'><i className="fa fa-address-card fa-lg"></i> Contact Us</NavLink>
                             </NavItem>
                             </Nav>
                         </Collapse>
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -3,12 +3,16 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 // shared data
 import { DISHLIST } from '../shared/dishList';
+import { COMMENTLIST } from '../shared/commentList';
+import { PROMOTIONLIST } from '../shared/promotionList';
+import { LEADERLIST } from '../shared/leaderList';
 
 // import  components
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 
 class MainComponent extends Component {
@@ -19,7 +23,10 @@ class MainComponent extends Component {
 
         // add dish list to the apps state
         this.state = {
-        dishList: DISHLIST
+        dishList: DISHLIST,
+        commentList: COMMENTLIST,
+        promotionList: PROMOTIONLIST,
+        leaderList: LEADERLIST
         };
 
     }
@@ -31,12 +38,21 @@ class MainComponent extends Component {
             <Header></Header>
                 <Switch>
                     <Route path='/home' component={
-                        () => <Home></Home>}
+                        () => <Home
+                                dish={this.state.dishList.filter((dish) => dish.featured)[0]}
+                                promotion={this.state.promotionList.filter((promo) => promo.featured)[0]}
+                                leader={this.state.leaderList.filter((leader) => leader.featured)[0]}
+                            ></Home>}
                     >
                     </Route>
                     <Route exact path='/menu' component={
                         () => <Menu dishList={this.state.dishList}></Menu>}
                     >
+                    </Route>
+                    <Route exact path='/contactus' component={
+                        () => <Contact></Contact>}
+                    >
+
                     </Route>
                     <Redirect to="/home" />
                 </Switch>
