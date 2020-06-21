e3b6eae558c903c847f33661d8a5ed5aefa350b0
Gabriel Jerome Solomon <gsolomon@scu.edu>
2020-06-21 14:58:43 -0700

    Added submit comment button to menu dish detail

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle, Button } from 'reactstrap';
 import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
-// disay dish details
+// comment form component
+function CommentForm(props) {
+
+    const { dishCommentList, toggleModal } = props;
+
+    return (
+        <React.Fragment>
+            <Button 
+                className="darkButton"
+                id="commentFormButton"
+                outline onClick={ () => { toggleModal(); } }
+            >
+            Submit Comment
+            </Button>
+        </React.Fragment>
+    )
+
+}
+
+// display dish details
 function RenderDish(props) {
 
     const { dish } = props;
@@ -31,7 +52,7 @@ function RenderDish(props) {
 // render the dish comments
 function RenderCommentList(props) {
 
-    const { dishCommentList } = props;
+    const { dishCommentList, toggleModal } = props;
 
     // date.toLocaleDateString('en-US', options));
     // <pre>{JSON.stringify(commentFormatted)}</pre>
@@ -77,6 +98,11 @@ function RenderCommentList(props) {
                             {commentItems}
                         </ul>
                     </CardText> 
+                    <CommentForm 
+                        dishCommentList={dishCommentList}
+                        toggleModal = { toggleModal }
+                    >
+                    </CommentForm>
                 </Card>
             </React.Fragment>
         );
@@ -87,39 +113,113 @@ function RenderCommentList(props) {
 }
 
 
-const DishDetail = (props) => {
+class DishDetail extends Component {
 
-    const { dish, dishCommentList } = props;
+    constructor(props) {
+        
+        super(props);
+    
+        this.state = {
+          isModalOpen: false,
+          dish: props.dish,
+          dishCommentList: props.dishCommentList
+        };
+
+        this.handleLogin = this.handleLogin.bind(this);
+
+      }
+
+    toggleModal() {
+
+        this.setState({ 
+            isModalOpen: !this.state.isModalOpen
+        });
+
+    }
+
+    handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
+    }
+
+    
 
     // console.log(props);
 
-    if (dish != null) {
-        return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-            </div>
-            <div className="row">
-                <div className="col-12">
-                        <h3>{dish.name}</h3>
+    render() {
+
+        let dish = this.state.dish;
+        let dishCommentList = this.state.dishCommentList;
+
+        if (dish != null) {
+            return (
+            <React.Fragment>
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                                <h3>{dish.name}</h3>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            <RenderDish dish={dish}></RenderDish>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <RenderCommentList 
+                                dishCommentList={dishCommentList}
+                                toggleModal={ () => { this.toggleModal(); } }
+                            >
+
+                                </RenderCommentList>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-12 col-md-6">
-                    <RenderDish dish={dish}></RenderDish>
-                </div>
-                <div className="col-12 col-md-6">
-                    <RenderCommentList dishCommentList={dishCommentList}></RenderCommentList>
-                </div>
-            </div>
-        </div>
-        )
-    } else {
-        return (<div></div>);
+                <Modal
+                    isOpen={this.state.isModalOpen}
+                    toggle={ () => { this.toggleModal(); } }
+                >
+                    <ModalHeader toggle={ () => { this.toggleModal(); } }>
+                        Login
+                    </ModalHeader>
+                    <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+            )
+        } else {
+            return (<div></div>);
+        }
+
     }
 
 }
