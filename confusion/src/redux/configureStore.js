1ae9b49b4acfff48f33ab9082f96e9824776ab92
Gabriel Jerome Solomon <gsolomon@scu.edu>
2020-06-20 19:05:40 -0700

    Added code to support redux dev tools for debugging things!  NICE!

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,11 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import { Reducer, initialState } from './reducer';
 
+// add redux dev tools
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+);
 
 // function to configure redux-react store
 export const ConfigureStore = () => {
 
-    const store = createStore(Reducer, initialState);
+    const store = createStore(Reducer, initialState, enhancers);
 
     return store;
 
