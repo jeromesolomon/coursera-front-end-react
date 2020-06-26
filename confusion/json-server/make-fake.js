d2a68152c52b5be2ce6a66a569ec30aceccea85e
Gabriel Jerome Solomon <gsolomon@scu.edu>
2020-06-25 22:57:58 -0700

    Added json-server, lodash, and faker modules for generating fake data

---
+++ confusion/json-server/make-fake.js
@@ -0,0 +1,43 @@
+
+const faker = require("faker");
+const _ = require("lodash");
+
+// set to us english
+faker.locale = "en_US";
+
+var randomName = faker.name.findName(); // Rowan Nikolaus
+var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+var randomImage = faker.image.image(); // random contact card containing many properties
+
+console.log("randomName =", randomName);
+console.log("randomEmail =", randomEmail);
+console.log("randomImage =", randomImage);
+
+let myObj = {
+    name: randomName,
+    email: randomEmail
+};
+
+console.log("myObj =", myObj);
+
+// 10 fake objects using lodash
+    
+let data = [];
+
+_.times(10, ()=> {
+    
+    const randomName = faker.name.findName();
+    const randomEmail = faker.internet.email();
+
+    data.push({
+        name: randomName,
+        email: randomEmail
+    });
+
+});
+
+console.log("data =", JSON.stringify(data, null, 2));
+
+
+
+
