2d795d5263d5ff528df62e733df458ef1b981d49
Gabriel Jerome Solomon <gsolomon@scu.edu>
2020-07-04 08:42:16 -0700

    Removed config info

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -5,29 +5,12 @@ import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 import './App.css';
 
-// add firebase
-import * as firebase from 'firebase';
-
 // add react redux
 // provider provides store to all components in the app via props
 import { Provider } from 'react-redux';
 
 // configure and create the react-redux store
 import { ConfigureStore } from './redux/configureStore';
-
-// create firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyA0gJo3B8VNFaQaPBJg2pukfJCxITxweOs",
-  authDomain: "reactaurant-361ee.firebaseapp.com",
-  databaseURL: "https://reactaurant-361ee.firebaseio.com",
-  projectId: "reactaurant-361ee",
-  storageBucket: "reactaurant-361ee.appspot.com",
-  messagingSenderId: "173192635918",
-  appId: "1:173192635918:web:1aa096d3224e15f00dbe5f",
-  measurementId: "G-1G3CZ8W0TS"
-};
-
-firebase.initializeApp(firebaseConfig);
 
 // create redux store
 const store = ConfigureStore();
